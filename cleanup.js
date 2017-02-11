65c8077a22b7dd8fa10f92a292be260482a3b35d
Maddie Revill <maddierevill@gmail.com>
2017-02-11 11:29:12 -0800

    Test simplified map

--- cleanup.js
+++ cleanup.js
@@ -1,4 +1,7 @@
 const featCleanup = (feature) => {
+
+
+
   let str = JSON.stringify(feature.properties);
   let stateMatch = /state:(..)/.exec(str) || /"([A-Z]{2})"/.exec(str);
   let distMatch = /cd:(\d\d?)/.exec(str) || /(\d\d?)(th|nd|st|rd)/.exec(str);
@@ -11,10 +14,24 @@ const featCleanup = (feature) => {
   else return false;
 };
 
+const atLarge = feature => {
+  let str = JSON.stringify(feature.properties);
+  let stateMatch = /("AK"|"DE"|"MT"|"ND"|"SD"|"VT"|"WY")/.exec(str);
+  if(stateMatch) {
+    let state = stateMatch[1];
+    let district = 0;
+    feature.properties = {state, district};
+    return true;
+  }
+  else return false;
+};
+
 const objCleanup = obj => {
   let unclean = [];
   obj.features.forEach(feat => {
-    if(!featCleanup(feat)) unclean.push(feat);
+    if(!featCleanup(feat)) {
+      if(!atLarge(feat)) unclean.push(feat);
+    }
   });
   return unclean;
 
