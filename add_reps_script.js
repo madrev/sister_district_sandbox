0445c4eb93b4f89a1fe2597bb1f48d77e9f3b7fb
Maddie Revill <maddierevill@gmail.com>
2017-02-11 20:22:28 -0800

    More data cleanup

--- add_reps_script.js
+++ add_reps_script.js
@@ -1,9 +1,10 @@
-const data = require('./simplified_tiger.json');
+const data = require('./reps_added.json');
 const fetch = require('node-fetch');
 const fs = require('fs');
 
 
 const fetchAndAdd = (feature) => {
+  if(feature.properties["REP"]) return true;
   let lat = feature.properties["INTPTLAT"];
   let lng = feature.properties["INTPTLON"];
   let url = `http://congress.api.sunlightfoundation.com/legislators/locate?latitude=${lat}&longitude=${lng}`;
@@ -11,13 +12,11 @@ const fetchAndAdd = (feature) => {
     .then(function(res) {
         return res.json();
     }).then(function(json) {
+        if(!json.results) console.log(json);
         json.results.forEach(result => {
           if(result.chamber === "house") {
           feature.properties["REP"] = result;
           console.log(feature.properties);
-        } else {
-          console.log(`${lat},${lng}`);
-          console.log(result);
         }
         });
     }).catch(err => console.log(err));
